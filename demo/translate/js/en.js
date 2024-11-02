f295f89817361ecb41387be11ba2fdcdff8704c7
Марухленко Анатолий <proxy33@mail.ru>
2024-11-02 16:39:56 +0300

    added js translate lab

---
+++ demo/translate/js/en.js
@@ -0,0 +1,7 @@
+export default {
+    translation: {
+      text: "hello world",
+      changeLang: "change language",
+    },
+  };
+  
---
+++ demo/translate/js/ru.js
@@ -0,0 +1,7 @@
+export default {
+    translation: {
+      text: "привет мир",
+      changeLang: "сменить язык",
+    },
+  };
+  
