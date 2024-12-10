058877317100be75aa1439acef095759efe8bccc
Марухленко Анатолий <proxy33@mail.ru>
2024-12-10 12:53:11 +0300

    stones

--- python/main_01.py
+++ python/main_01.py
@@ -1,3 +1,27 @@
+def F(n):
+    if n>1:
+        print(n)
+        F(n-3)
+        F(n-2)
+F(13)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # a,b=map(int,input().split())
 # print(a+b)
 
@@ -13,18 +37,33 @@
 # f.close()
 
 
-j=input()
-jewels = set(list(j))
+# j=input()
+# jewels = set(list(j))
 
-s=input()
-stones = list(s)
+# s=input()
+# stones = list(s)
 
-count = 0
-for stone in stones:
-    if stone in jewels:
-        count+=1
+# count = 0
+# for stone in stones:
+#     if stone in jewels:
+#         count+=1
 
-print(count)
+# print(count)
+
+
+# def main():
+#     nums = [1,2,3,4,5,6,7]
+#     k = 3
+    
+#     r=[]
+#     for i in range(len(nums)):
+#         r.append(nums[(i-k)%len(nums)])
+    
+#     for i in range(len(nums)):
+#         nums[i]=r[i]
+    
+#     return r
 
 
 
+# print(main())
