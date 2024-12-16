2fb41b7879b7840b3fa58732b20f4d6e5ef22ce7
malsoft-dev-git <ibswsu1@gmail.com>
2024-12-16 18:45:33 +0300

    Бобкова Кристина выполнила 10 лабораторную

---
+++ 65451/10/my/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import include, path
+from .views import *
+
+urlpatterns = [
+    path('', list,name='url_list'), 
+    path('create',  create, name='url_create'),  
+    path('preview/<id>',  preview),       
+    path('update/<id>',  update),   
+    path('delete/<int:id>',  destroy),
+    path('news', news, name='url_news') 
+]
