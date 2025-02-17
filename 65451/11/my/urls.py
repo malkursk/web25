57179da848569f22a8fb8c9a18408ed7ed9a5fbb
malsoft-dev-git <ibswsu1@gmail.com>
2025-02-17 17:58:24 +0300

    Бобкова Кристина выполнила 11 практику

---
+++ 65451/11/my/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import include, path
+from .views import *
+
+urlpatterns = [
+    path('', list,name='url_list'), 
+    path('create',  create, name='url_create'),  
+    path('preview/<id>',  preview),       
+    path('update/<id>',  update),   
+    path('delete/<int:id>',  destroy),
+    path('inform', about, name='url_inform'),
+    path('krik', bob, name='url_krik'),
+    path('news', news, name='url_news') 
+]
---
+++ 65451/11/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('my.urls')),
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
